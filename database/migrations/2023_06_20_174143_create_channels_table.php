8fbd884bb98074734b98449b3b656a62a8c2e70c
mohammedterfa <mohammedterfa@gmail.com>
2023-06-20 20:49:23 +0300

    Channel migration created

---
+++ database/migrations/2023_06_20_174143_create_channels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('channels', static function (Blueprint $table) {
+            $table->ulid('id')->primary();
+
+            $table->string('name');
+
+            $table
+                ->foreignUlid('project_id')
+                ->index()
+                ->constrained()
+                ->cascadeOnDelete();
+
+
+            $table->timestamps();
+        });
+    }
+
+
+    public function down(): void
+    {
+        Schema::dropIfExists('channels');
+    }
+};
