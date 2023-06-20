f9fe1dc523db3511cba4c6aa09d1c7b5b82a8cf8
mohammedterfa <mohammedterfa@gmail.com>
2023-06-20 10:00:31 +0300

    edit factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,25 +10,19 @@ use Laravel\Jetstream\Features;
 
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
+
     protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+
     public function definition(): array
     {
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make(
+                value: 'password',
+            ),
             'two_factor_secret' => null,
             'two_factor_recovery_codes' => null,
             'remember_token' => Str::random(10),
@@ -37,10 +31,8 @@ class UserFactory extends Factory
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
+
+    public function unverified(): UserFactory
     {
         return $this->state(function (array $attributes) {
             return [
