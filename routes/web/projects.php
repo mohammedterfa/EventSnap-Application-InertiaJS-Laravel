30b2e8ad317300f202f3005d678d703a99757387
mohammedterfa <mohammedterfa@gmail.com>
2023-06-24 18:46:59 +0300

    create projects routes

---
+++ routes/web/projects.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/')->name('index');
+Route::post('/')->name('store');
+
--- routes/web/routes.php
+++ routes/web/routes.php
@@ -6,15 +6,23 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::middleware(['auth:sanctum', config("jetstream.auth_session"),])->group(static function (): void {
-    Route::get('/', function () {
-        return Inertia::render('Welcome', [
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
-            'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
-        ]);
-    });
+    Route::middleware(['auth:sanctum', config("jetstream.auth_session"),])->group(static function (): void {
+        Route::get('/', function () {
+            return Inertia::render('Welcome', [
+                'canLogin' => Route::has('login'),
+                'canRegister' => Route::has('register'),
+                'laravelVersion' => Application::VERSION,
+                'phpVersion' => PHP_VERSION,
+            ]);
+        });
 
     Route::get('/dashboard', fn () => Inertia::render('Dashboard'))->name('dashboard');
+
+    /**
+     * Project Routes
+     */
+
+    Route::prefix('projects')->as('projects:')->group(
+        base_path('routes/web/projects.php'),
+    );
 });
