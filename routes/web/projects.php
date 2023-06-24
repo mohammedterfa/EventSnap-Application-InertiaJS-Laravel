e4783d0f8ab8cbe02c6b4f845cd1f50e9c944c67
mohammedterfa <mohammedterfa@gmail.com>
2023-06-24 18:52:00 +0300

    Index, Store Controllers and tests created

--- routes/web/projects.php
+++ routes/web/projects.php
@@ -2,10 +2,11 @@
 
 declare(strict_types=1);
 
-
+use App\Http\Controllers\Web\Projects\IndexController;
+use App\Http\Controllers\Web\Projects\StoreController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/')->name('index');
-Route::post('/')->name('store');
+Route::get('/' , IndexController::class)->name('index');
+Route::post('/', StoreController::class)->name('store');
 
