7e5bfce19b6d33ba569d148f766fbe115f7d5dc1
mohammedterfa <mohammedterfa@gmail.com>
2023-07-25 20:13:25 +0300

    show projects in projects page

---
+++ resources/js/Components/Header.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  heading: {
+    type: String,
+    required: true,
+  },
+
+  message: {
+    type: String,
+    required: true,
+
+  }
+})
+</script>
+<template>
+<div class="border-b border-gray-200 pb-5 sm:flex sm:items-baseline
+ justify-between">
+  <h3>{{ heading }}</h3>
+  <div class="mt-3 sm:ml-4 sm:mt-0">
+    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+      {{ message }}
+    </button>
+  </div>
+</div>
+
+</template>
--- resources/js/Pages/Projects/Index.vue
+++ resources/js/Pages/Projects/Index.vue
@@ -1,10 +1,58 @@
 <script setup>
 import Application from '@/Layouts/Application.vue';
+import  Container  from '@/Components/Container.vue';
+import Header from '@/Components/Header.vue';
+
+defineProps({
+  projects: {
+    type: Array,
+    required: true,
+  }
+})
+
+const statues = {
+  active: 'text-green-700 bg-green-50 ring-green-600/20',
+  inactive: 'text-yallow-800 bg-yellow-50 ring-yellow-600/20'
+}
 </script>
 
 
 <template>
   <Application>
-    guierhguihreu
+    <Container>
+      <section class="py-6">
+        <Header
+          heading="Your Team Projects"
+          message="Create New Project"
+        />
+      </section>
+
+      <section  class="py-6">
+        <ul role="list" class="divide-y divide-gray-100">
+          <li v-for="project in projects" :key="project.id" class="flex items-center justify-between">
+            <div class="min-w-0">
+              <div class="flex items-start gap-x-3">
+                <p class="text-sm font-semibold leading-6 text-gray-900">
+                  {{ project.name }}
+                </p>
+
+                <p
+                  class="text-green-700 bg-green-50 ring-green-600/20"
+                >
+                  {{ project.status }}
+                </p>
+              </div>
+
+              <div class="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+                <p class="whitespace-nowrap">
+                  Due on
+                  {{ project.dueDate }}
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </Container>
   </Application>
 </template>
